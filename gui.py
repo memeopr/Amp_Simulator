b9e3bf7a01a00356be96f0b729948dc56beeb050
memeopr <memeopr@gmail.com>
2023-04-14 18:11:03 -0400

    Added functionality to calculate the levels using  Low and High Pilots

--- gui.py
+++ gui.py
@@ -11,16 +11,18 @@ input_size = (15, 1)
 
 sg.theme("DarkTeal2")
 
-label = sg.Text("Enter High Frequency (MHz)")
+use_two_pilots_checkbox = sg.Checkbox("Use High/Low pilots method instead.", key="use_two_pilots", enable_events=True)
+
+label = sg.Text("Enter High Frequency (MHz)", key="label")
 high_freq = sg.InputText(tooltip="Enter High Frequency", key="high_freq", default_text="1218")
 
-label2 = sg.Text("Enter Tilt at High Frequency (dB)")
+label2 = sg.Text("Enter Tilt at High Frequency (dB)", key="label2")
 tilt_at_high_freq = sg.InputText(tooltip="Enter Tilt at High Frequency", key="tilt_at_high_freq", default_text="17")
 
-label3 = sg.Text("Enter Carrier level (dBmV)")
+label3 = sg.Text("Enter Carrier level (dBmV)", key="label3")
 carrier_level = sg.InputText(tooltip="Enter Carrier level", key="carrier_level", default_text="52")
 
-label4 = sg.Text("Enter Carrier Frequency (MHz)")
+label4 = sg.Text("Enter Carrier Frequency (MHz)", key="label4")
 carrier_freq = sg.InputText(tooltip="Enter Carrier Frequency", key="carrier_freq", default_text="1218")
 
 label5 = sg.Text("Frequency Split")
@@ -41,10 +43,10 @@ frame2 = sg.Frame("Find Level", [[sg.Text("   Frequency (MHz)"),
                                   sg.Text("", key="mystery_level")]], size=(640, 50))
 
 frame3 = sg.Frame("Find Tilt", [[sg.Text("         Pilot 1 (MHz)"),
-                                sg.InputText(tooltip="Enter Pilot #1", key="-pilot1-", enable_events=True,
-                                             size=input_size), sg.Text("Pilot 2 (MHz) "),
-                                sg.InputText(tooltip="Enter Pilot #2", key="-pilot2-", enable_events=True,
-                                             size=input_size), sg.Text("", key="mystery_tilt")]], size=(640, 50))
+                                 sg.InputText(tooltip="Enter Pilot #1", key="-pilot1-", enable_events=True,
+                                              size=input_size), sg.Text("Pilot 2 (MHz) "),
+                                 sg.InputText(tooltip="Enter Pilot #2", key="-pilot2-", enable_events=True,
+                                              size=input_size), sg.Text("", key="mystery_tilt")]], size=(640, 50))
 
 frame4 = sg.Frame("CH <-> Frequency Converter", [[sg.Text("Frequency (MHz)   "),
                                                   sg.InputText(tooltip="Enter Frequency (MHz)", key="convert_freq",
@@ -56,20 +58,20 @@ frame4 = sg.Frame("CH <-> Frequency Converter", [[sg.Text("Frequency (MHz)   "),
                                                   sg.Text("", key="CH_FREQ")]
                                                  ], size=(640, 75))
 column = sg.Column(scrollable=True, expand_x=False, expand_y=True, vertical_scroll_only=True,
-            layout=[[frame],
-                    [sg.Push(), label6, sg.Push()],
-                    [canvas],
-                    [sg.Push(), calculate, sg.Exit(key="Exit"), sg.Push()],
-                    [frame2],
-                    [frame3],
-                    [frame4]])
+                   layout=[[use_two_pilots_checkbox],
+                           [frame],
+                           [sg.Push(), label6, sg.Push()],
+                           [canvas],
+                           [sg.Push(), calculate, sg.Exit(key="Exit"), sg.Push()],
+                           [frame2],
+                           [frame3],
+                           [frame4]])
 
 window = sg.Window("System Levels by Tito Velez",
-                    layout=[[column]],
-                    finalize=True,
-                    resizable =True,
-                    font=("Helvetica", 10), icon=r"K:\PythonProjects\pythonProject\Amp_Simulator\icon.ico")
-
+                   layout=[[column]],
+                   finalize=True,
+                   resizable=True,
+                   font=("Helvetica", 10), icon=r"K:\PythonProjects\pythonProject\Amp_Simulator\icon.ico")
 
 # matplotlib
 fig = Figure(figsize=(6.40, 4.80))
@@ -83,41 +85,82 @@ while True:
     match event:
         case "calculate":
             try:
-                x, y = fu.system_levels(float(values['high_freq']), float(values['tilt_at_high_freq']),
-                                        float(values['carrier_level']), float(values['carrier_freq']), values['split'])
+                if values["use_two_pilots"]:
+                    new_tilt = float(values['tilt_at_high_freq']) - float(values['carrier_freq'])
 
-                fu.plot_levels(x, y, fig, figure_canvas_agg)
+                    x, y = fu.system_levels(float(values['high_freq']), new_tilt,
+                                            float(values['tilt_at_high_freq']), float(values['high_freq']),
+                                            values['split'])
 
-                window["-mystery_frequency-"].update("")
-                window["mystery_level"].update("")
-                window["mystery_tilt"].update("")
-                window["-pilot1-"].update("")
-                window["-pilot2-"].update("")
+                    fu.plot_levels(x, y, fig, figure_canvas_agg)
+
+                    window["-mystery_frequency-"].update("")
+                    window["mystery_level"].update("")
+                    window["mystery_tilt"].update("")
+                    window["-pilot1-"].update("")
+                    window["-pilot2-"].update("")
+
+                else:
+                    x, y = fu.system_levels(float(values['high_freq']), float(values['tilt_at_high_freq']),
+                                            float(values['carrier_level']), float(values['carrier_freq']),
+                                            values['split'])
+
+                    fu.plot_levels(x, y, fig, figure_canvas_agg)
+
+                    window["-mystery_frequency-"].update("")
+                    window["mystery_level"].update("")
+                    window["mystery_tilt"].update("")
+                    window["-pilot1-"].update("")
+                    window["-pilot2-"].update("")
             except ValueError:
                 sg.popup_error("Values must be numeric")
         case "-mystery_frequency-":
             freq = values["-mystery_frequency-"]
-            if freq.isnumeric():
-                z, w = fu.mystery_freq(float(values['high_freq']), float(values['tilt_at_high_freq']),
-                                       float(values['carrier_level']), float(values['carrier_freq']),
-                                       float(freq), values['split'])
-                window["mystery_level"].update(f"Level is : {round(w,2)} dBmV")
+            if values["use_two_pilots"]:
+                if freq.isnumeric():
+                    new_tilt = float(values['tilt_at_high_freq']) - float(values['carrier_freq'])
+                    z, w = fu.mystery_freq(float(values['high_freq']), new_tilt,
+                                           float(values['tilt_at_high_freq']), float(values['high_freq']),
+                                           float(freq), values['split'])
+                    window["mystery_level"].update(f"Level is : {round(w, 2)} dBmV")
+                else:
+                    window["mystery_level"].update("")
             else:
-                window["mystery_level"].update("")
+                if freq.isnumeric():
+                    z, w = fu.mystery_freq(float(values['high_freq']), float(values['tilt_at_high_freq']),
+                                           float(values['carrier_level']), float(values['carrier_freq']),
+                                           float(freq), values['split'])
+                    window["mystery_level"].update(f"Level is : {round(w, 2)} dBmV")
+                else:
+                    window["mystery_level"].update("")
         case ("-pilot1-" | "-pilot2-"):
             pilot1 = values["-pilot1-"]
             pilot2 = values["-pilot2-"]
-            if pilot1.isnumeric() and pilot2.isnumeric():
-                z1, w1 = fu.mystery_freq(float(values['high_freq']), float(values['tilt_at_high_freq']),
-                                         float(values['carrier_level']), float(values['carrier_freq']),
-                                         float(pilot1), values['split'])
-                z2, w2 = fu.mystery_freq(float(values['high_freq']), float(values['tilt_at_high_freq']),
-                                         float(values['carrier_level']), float(values['carrier_freq']),
-                                         float(pilot2), values['split'])
-                tilt = w2-w1
-                window["mystery_tilt"].update(f"Tilt is : {round(tilt, 2)} dB")
+            if values["use_two_pilots"]:
+                new_tilt = float(values['tilt_at_high_freq']) - float(values['carrier_freq'])
+                if pilot1.isnumeric() and pilot2.isnumeric():
+                    z1, w1 = fu.mystery_freq(float(values['high_freq']), new_tilt,
+                                             float(values['tilt_at_high_freq']), float(values['high_freq']),
+                                             float(pilot1), values['split'])
+                    z2, w2 = fu.mystery_freq(float(values['high_freq']), new_tilt,
+                                             float(values['tilt_at_high_freq']), float(values['high_freq']),
+                                             float(pilot2), values['split'])
+                    tilt = w2 - w1
+                    window["mystery_tilt"].update(f"Tilt is : {round(tilt, 2)} dB")
+                else:
+                    window["mystery_tilt"].update("")
             else:
-                window["mystery_tilt"].update("")
+                if pilot1.isnumeric() and pilot2.isnumeric():
+                    z1, w1 = fu.mystery_freq(float(values['high_freq']), float(values['tilt_at_high_freq']),
+                                             float(values['carrier_level']), float(values['carrier_freq']),
+                                             float(pilot1), values['split'])
+                    z2, w2 = fu.mystery_freq(float(values['high_freq']), float(values['tilt_at_high_freq']),
+                                             float(values['carrier_level']), float(values['carrier_freq']),
+                                             float(pilot2), values['split'])
+                    tilt = w2 - w1
+                    window["mystery_tilt"].update(f"Tilt is : {round(tilt, 2)} dB")
+                else:
+                    window["mystery_tilt"].update("")
         case "convert_freq":
             freq_to_convert = values["convert_freq"]
             if freq_to_convert.isnumeric():
@@ -132,11 +175,20 @@ while True:
                 window["CH_FREQ"].update(f"Frequency is  {freq_num} MHz")
             else:
                 window["CH_FREQ"].update("")
-
+        case "use_two_pilots":
+            if values["use_two_pilots"]:
+                window["label"].update("Enter High Pilot Frequency (MHz)")
+                window["label2"].update("Enter High Pilot Level (dBmV)")
+                window["label3"].update("Enter Low Pilot Frequency (MHz)")
+                window["label4"].update("Enter Low Pilot Level (dBmV)")
+            else:
+                window["label"].update("Enter High Frequency (MHz)")
+                window["label2"].update("Enter Tilt at High Frequency (dB)")
+                window["label3"].update("Enter Carrier level (dBmV)")
+                window["label4"].update("Enter Carrier Frequency (MHz)")
         case "Exit":
             break
         case sg.WIN_CLOSED:
             break
-
 
 window.close()
